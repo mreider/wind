f67a106b7bfb36c43d444745d33f15a586267783
Matt Reider <mreider@gmail.com>
2014-03-27 15:36:50 -0700

    fixed db

--- config.rb
+++ config.rb
@@ -7,13 +7,19 @@ require_relative 'lib/atstart.rb'
 require_relative 'lib/datasync.rb'
 require_relative 'lib/extend_string.rb'
 
-require 'cfruntime/mysql'
+db = JSON.parse(ENV['VCAP_SERVICES'])["p-mysql"]
+  credentials = db.first["credentials"]
+  host = credentials["hostname"]
+  username = credentials["username"]
+  password = credentials["password"]
+  database = credentials["name"]
+  port = credentials["port"]
 
 # Plugins.
 Dir['plugins/*.rb'].each { |plugin| require_relative plugin }  
 
 # Database connection.
-DB = Sequel.connect(:adapter=>'mysql2', :host=>'localhost', :database=>'wind', :user=>'root', :password=>'')
+DB = Sequel.connect(:adapter=>'mysql2', :host=>host, :database=>database, :user=>username, :password=>password, :port=>port)
 
 # Sequel schema plugin.
 Sequel::Model.plugin :schema
